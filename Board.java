193cb76e691de668383396351e446188bbdca4ce
pulibhavana <bhavana.puli@thoughtworks.com>
2020-01-31 10:45:41 +0530

    modify TicTacToe.java

---
+++ Board.java
@@ -0,0 +1,97 @@
+package tw_games;
+
+class Board 
+{
+	int flag=0;
+	public boolean gameends(String[] grid)
+	{
+		flag=0;
+		for(int index=0;index<9;index++)
+		{
+			if(grid[index]==null)
+			{
+				flag=1;
+			}
+			
+				
+		}
+		if(flag==0)
+			System.out.println("Draw");
+		return (flag==0);
+	}
+	public boolean row_check(String[] grid)
+	{
+		flag=0;
+		for(int rowIndex=0;rowIndex<9;rowIndex+=3)
+		{
+			if(grid[rowIndex]!=null)
+			{
+				String text=grid[rowIndex];
+				if(grid[rowIndex+1]==text && grid[rowIndex+2]==text)
+				{
+					System.out.println(text+"is winner");
+					flag=1;
+				}
+			}
+			
+		}
+		return (flag==1);
+		
+	}
+	
+	public boolean col_check(String[] grid)
+	{
+		flag=0;
+		for(int colIndex=0;colIndex<3;colIndex++)
+		{
+			if(grid[colIndex]!=null)
+			{
+				String text=grid[colIndex];
+				if(grid[colIndex+3]==text && grid[colIndex+6]==text)
+				{
+					System.out.println(text+"is winner");
+					flag=1;
+				}
+			}
+			
+		}
+		return (flag==1);
+		
+	}
+	public boolean left_diagonal_check(String[] grid)
+	{
+		flag=0;
+		if(grid[0]!=null)
+		{
+			String text=grid[0];
+			if(grid[4]==text && grid[8]==text)
+			{
+				System.out.println(text+"is winner");
+				flag=1;
+			}
+		}
+		return (flag==1);
+		
+	}
+	public boolean right_diagonal_check(String[] grid)
+	{
+		flag=0;
+		if(grid[2]!=null)
+		{
+			String text=grid[2];
+			if(grid[4]==text && grid[6]==text)
+			{
+				System.out.println(text+"is winner");
+				flag=1;
+			}
+		}
+		return (flag==1);
+		
+	}
+	
+		
+		
+		
+		
+}
+
--- TicTacToe.java
+++ TicTacToe.java
@@ -3,78 +3,58 @@ package tw_games;
 public class TicTacToe  {
 
 	String[] grid=new String[9];
-	int[] validMoves=new int[9];
-	int chance=0,choose;
+	int chance=0,position;
 	players p1=new players(1);
 	players p2=new players(2);
-	boolean rowFlag,colFlag,ldFlag,rdFlag,movesFlag;
-	GameResults gp=new GameResults();
+	Board bo=new Board();
 	public void inputFromPlayer()
 	{
 		while(!gameResults())
 		{
-			if(chance%2==0 && chance<9)
+			switch(chance%2)
 			{
-				player1validate();
-				chance++;
-			}
-			else
-			{
-				if(chance<9)
-				{
-					player2validate();
-					chance++;
-				}
+				case 0: position=p1.player_input();
+						while(!validateInput(position))
+						{
+							System.out.println("the position is filled!");
+							position=p1.player_input();
+						}
+						grid[position]=p1.variable;
+						chance++;
+						break;
+				
+				case 1: position=p2.player_input();
+						while(!validateInput(position))
+						{
+							System.out.println("the position is filled!");
+							position=p1.player_input();
+
+						}
+						grid[position]=p2.variable;
+						chance++;
+						break;
 				
 			}
-			
-			
 		}
 	}
 	
-	public void player1validate()
-	{
-		choose=p1.player_input(1);
-		while(!validateInput(choose))
-		{
-			System.out.print("choose another position");
-			p1.player_input(1);
-		}
-		grid[choose]=p1.variable;
-		
-	}
-	
-	public void player2validate()
-	{
-		choose=p2.player_input(2);
-		while(!validateInput(choose))
-		{
-			System.out.println(" The position is filled \n "
-					+ "choose another position");
-			p2.player_input(2);
-		}
-		grid[choose]=p2.variable;
-	}
 	
 	public boolean validateInput(int input)
 	{
-		if(validMoves[input]==0)
-		{
-			validMoves[input]=1;
+		if(grid[input]==null)
 			return true;
-		}
-		else
-			return false;
+		return false;
 		
 	}
 	public boolean gameResults()
 	{
+		boolean rowFlag,colFlag,ldFlag,rdFlag,movesFlag;
 		
-		movesFlag=gp.gameends(grid);
-		rowFlag=gp.row_check(grid);
-		colFlag=gp.col_check(grid);
-		ldFlag=gp.left_diagonal_check(grid);
-		rdFlag=gp.right_diagonal_check(grid);
+		movesFlag=bo.gameends(grid);
+		rowFlag=bo.row_check(grid);
+		colFlag=bo.col_check(grid);
+		ldFlag=bo.left_diagonal_check(grid);
+		rdFlag=bo.right_diagonal_check(grid);
 		
 		
 		return (movesFlag||rowFlag || colFlag ||ldFlag || rdFlag);
